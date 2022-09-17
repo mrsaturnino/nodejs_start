10c9d4dc977dc068c1201afbc89e590232500460
mrsaturnino <andre_santos2012@hotmail.com>
2022-09-17 09:24:53 -0300

    Estruturas condicionais com o EJS

--- guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
+++ guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
@@ -8,13 +8,15 @@ app.set('view engine', 'ejs'); //Passando para o Express o motor de template (vi
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false;
     res.render("index",{ //O método "render()" faz uma verificação dentro da pasta "views" para renderizar o arquivo inserido.
         
-        nome: nome, //Também podemos criar objetos, inserir variáveis neles e exibir no arquivo HTML com o render().
-        lang: lang, 
-        cargo: "Programador Backend Node.js"
-
+        //Também podemos criar objetos, inserir variáveis neles e exibir no arquivo HTML com o render().
         //basta declarar dentro do "  render("pagina.js", {var = valor})  " e no arquivo HTML, inserir com a tag "<%= %>"
+        nome: nome,
+        lang: lang, 
+        cargo: "Programador Backend Node.js",
+        msg: exibirMsg //valor booleano para estrutura de repetição que está no EJS.
     })
 });
 
