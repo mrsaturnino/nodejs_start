7f58e65685d159a5e4dff03d04afa76a8231104c
mrsaturnino <andre_santos2012@hotmail.com>
2022-09-17 08:19:57 -0300

    Exibindo variaveis no HTML com o EJS

--- guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
+++ guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
@@ -3,10 +3,19 @@ const app = express();
 
 app.set('view engine', 'ejs'); //Passando para o Express o motor de template (view engine) que utilizaremos, que é o "ejs".
 
-//criando uma rota de teste
+//criando uma rota
 
-app.get("/", (req, res) => {
-    res.render("index") //O método "render()" faz uma verificação dentro da pasta "views" para renderizar o arquivo inserido.
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index",{ //O método "render()" faz uma verificação dentro da pasta "views" para renderizar o arquivo inserido.
+        
+        nome: nome, //Também podemos criar objetos, inserir variáveis neles e exibir no arquivo HTML com o render().
+        lang: lang, 
+        cargo: "Programador Backend Node.js"
+
+        //basta declarar dentro do "  render("pagina.js", {var = valor})  " e no arquivo HTML, inserir com a tag "<%= %>"
+    })
 });
 
 
