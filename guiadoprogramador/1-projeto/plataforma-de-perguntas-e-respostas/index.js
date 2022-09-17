2a682227ef2342a968e8c6056d61478a6e24ed17
mrsaturnino <andre_santos2012@hotmail.com>
2022-09-17 09:50:42 -0300

    Estruturas de repetição com o EJS

--- guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
+++ guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
@@ -6,9 +6,19 @@ app.set('view engine', 'ejs'); //Passando para o Express o motor de template (vi
 //criando uma rota
 
 app.get("/:nome/:lang", (req, res) => {
+    
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false;
+
+    var produtos = [ //Criando uma array de objetos que será utilizada na estrutura de repetição do arquivo EJS.
+        {nome: "Bola de Basquete", preco: 89.99},
+        {nome: "Tênis Nike Air Precision VI", preco: 320.00},
+        {nome: "Camiseta Adidas Performance Dry Fit", preco: 79.99},
+        {nome: "SmartWatch AmazFIT 5.0", preco: 599.99 },
+        {nome: "SSD NVme Kingston 500GB M.2", preco: 279.99}
+    ]
+
     res.render("index",{ //O método "render()" faz uma verificação dentro da pasta "views" para renderizar o arquivo inserido.
         
         //Também podemos criar objetos, inserir variáveis neles e exibir no arquivo HTML com o render().
@@ -16,7 +26,8 @@ app.get("/:nome/:lang", (req, res) => {
         nome: nome,
         lang: lang, 
         cargo: "Programador Backend Node.js",
-        msg: exibirMsg //valor booleano para estrutura de repetição que está no EJS.
+        msg: exibirMsg, //valor booleano para estrutura condicional que está no EJS.
+        produtos: produtos //inserindo a array de objetos "produtos" na renderização da página.
     })
 });
 
