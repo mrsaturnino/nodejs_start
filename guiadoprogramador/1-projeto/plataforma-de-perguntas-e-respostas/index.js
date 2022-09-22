765a519637575a84bb6bb51b80c47709453a8a51
André Saturnino <andre_santos2012@hotmail.com>
2022-09-22 13:14:27 -0300

    Aplicando estilos do bootstrap no projeto

--- guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
+++ guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
@@ -10,6 +10,9 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/perguntar", (req, res) => { //Rota para renderização da página "perguntar.ejs"
+    res.render("perguntar.ejs")
+});
 
 app.listen(4040, ()=> { //Abrindo o server na porta 4040.
     console.log("App rodando!")
