4f3cb40538ccf96cc822cc59b93001c861d0bd6d
mrsaturnino <andre_santos2012@hotmail.com>
2022-09-24 19:37:04 -0300

    Enviando formulário para o BackEnd e capturando dados com o BodyParser (Expressjs)

--- guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
+++ guiadoprogramador/1-projeto/plataforma-de-perguntas-e-respostas/index.js
@@ -1,10 +1,17 @@
 const express = require("express"); //importando o express para o projeto
 const app = express();
+const bodyParser = require("body-parser"); //importando o Body Parser para captura de dados enviados pelo formulário
 
-app.set('view engine', 'ejs'); //Passando para o Express o motor de template (view engine) que utilizaremos, que é o "ejs".
-
+//Passando para o Express o motor de template (view engine) que utilizaremos, que é o "ejs".
+app.set('view engine', 'ejs'); 
 app.use(express.static('public'));
 
+//BODY PARSER
+app.use(bodyParser.urlencoded({extended: false})); //O método "urlenconded()" do body parser decodifica os dados enviados pelo formulário em uma estrutura JavaScript.
+app.use(bodyParser.json()); //O método "json()"" do body parser faz com que os dados de formulário venham em formato JSON.
+
+
+//ROTAS
 app.get("/", (req, res) => {
 
     res.render("index");
@@ -12,6 +19,12 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => { //Rota para renderização da página "perguntar.ejs"
     res.render("perguntar.ejs")
+});
+
+app.post("/dadosformulario", (req, res) => { //Rota para receber os dados preenchidos do formulário por meio de uma rota do tipo POST.
+    var title = req.body.title; //capturando os dados do formulário com o body-parser
+    var description = req.body.description;
+    res.send("Dados do formulário recebidos! " + "Título da pergunta: " + title + " Descrição: " + description)
 });
 
 app.listen(4040, ()=> { //Abrindo o server na porta 4040.
